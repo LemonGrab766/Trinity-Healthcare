e8da0d15179ce5ea49abaffdd563f6dee26488a9
LemonGrab766 <lemongrab766@gmail.com>
2024-04-20 18:38:11 -0300

    fix banner-home

--- src/components/Banners/HomeBanner.js
+++ src/components/Banners/HomeBanner.js
@@ -4,8 +4,7 @@ import Nav from "../Nav/Nav";
 export default function HomeBanner() {
   return (
     <div className=" bg-home">
-      {/* <div className=" bg-[#00aaa4af]"> */}
-      <div>
+      <div className=" bg-[#00aaa4af]">
         <Nav />
         <div className=" flex flex-col justify-center items-center py-60">
           <h1 className=" text-[60px] text-white font-bold">

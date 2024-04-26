ce9dffa3b0e827b3a502842c08788538df7758da
LemonGrab766 <lemongrab766@gmail.com>
2024-04-26 12:17:30 -0300

    envs send email

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -7,14 +7,14 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST(req) {
   try {
     const { name, email, message } = await req.json();
-    const data = await resend.emails.send({
-      from: "Trinity Page <onboarding@resend.dev>",
-      to: ["trinityhealthcareservices51@gmail.com"],
+    await resend.emails.send({
+      from: process.env.EMAIL_FROM,
+      to: [process.env.EMAIL_TO],
       subject: "Hello world",
       react: EmailTemplate({ name, email, message }),
     });
 
-    return Response.json({message:"Message sent"});
+    return Response.json({ message: "Message sent" });
   } catch (error) {
     return Response.json({ error });
   }
